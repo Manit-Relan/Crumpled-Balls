00fce71dcd972552a2e73e18d99166188dbd5f2a
Manit-Relan <75022063+Manit-Relan@users.noreply.github.com>
2021-01-24 11:56:20 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -3,12 +3,12 @@ class Paper {
     var options = {
       isStatic : false,
       restitution:0.2,
-      firction: 0.3,
+      friction: 0.3,
       density:1.0
     }
     this.radius = radius
-    this.body = Bodies.circle(x,y,radius,options)
-    World.add(world,this.body)
+    this.body = Bodies.circle(x,y,radius,options);
+    World.add(world,this.body);
   }
   display(){
     fill("yellow")
